e4b79948bee3f85c5d0326ab3e50f5adf83696ef
margb13 <marg.b13@hotmail.com>
2018-04-09 08:35:25 -0500

    Mejora de vistas

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 /* Efecto del scroll */
-
+/*
 $(window).scroll(function() {
-  if ($('#navbar-portafolio').offset().top > 300) {
+  if ($('#navbar-portafolio').offset().top > 5) {
     $('#navbar-portafolio').addClass('bg-dark');
     $('#navbar-portafolio').removeClass('transparent');
   } else {
     $('#navbar-portafolio').removeClass('bg-dark');
     $('#navbar-portafolio').addClass('transparent');
   }
-});
+});*/
 
+$(document).ready(function() {
+  // Transition effect for navbar
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 280px, adds/removes bg-dark/transparent class
+    if ($(this).scrollTop() > 280) {
+      $('#navbar-portafolio').addClass('bg-dark');
+      $('#navbar-portafolio').removeClass('transparent');
+    } else {
+      $('#navbar-portafolio').addClass('transparent');
+      $('#navbar-portafolio').removeClass('bg-dark');
+    }
+  });
+});
